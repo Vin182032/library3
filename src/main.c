6bc885882826c08b89b1bf23ed954e3bf5307a7a
root <root@localhost>
2024-10-03 23:48:57 +0300

    auto publish 1727988537126

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+int overdue;
+int fine;
+int return;
+int due;
+
+printf("enter id");
+scanf("%d",id);
+
+printf("enter due");
+scanf("%d",due);
+
+printf("enter return");
+scanf("%d", return);
+
+overdie=return=due
+
+printf("overdue days are %d");
+if(overdue<=7){
+Fine=overdue*20;
+ printf("fine rate is Ksh 20");
+ printf("fine amount is%d",fine);}
+ if(overdue>=8){
+ fine=overdue*50;
+ printf("fine rate is Ksh 50");
+ printf("fine amount is Ksh %d",fine);
+ 
+ return 0;
+}
